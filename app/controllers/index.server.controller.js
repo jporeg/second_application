0a9be3c499de4a8619ba9fd96176552777aa8129
jporeg <jporegero@gmail.com>
2024-07-25 07:50:17 -0400

    first commit

---
+++ app/controllers/index.server.controller.js
@@ -0,0 +1,8 @@
+exports.render =
+function (req, res) {
+if (req.session.lastVisit) {
+console.log(req.session.lastVisit);
+}
+req.session.lastVisit = new Date();
+res.render('index', { title: 'Joseph Oregero JOSORE9146' });
+};
